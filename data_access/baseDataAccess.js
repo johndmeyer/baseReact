c10a6d92242fd96ec19de090a571c483b2e16ca2
John Meyer <john.meyer@ihsmarkit.com>
2018-12-20 09:47:20 -0700

    Clean up and todos

--- data_access/baseDataAccess.js
+++ data_access/baseDataAccess.js
@@ -8,6 +8,7 @@ const baseUrl = 'https://nab-api.markitqa.com/nab-mobile-responsive/1.0';
 //const baseUrl = 'https://nab.api.markitondemand.com/nab-mobile-responsive/1.0';
 
 const getAccessToken = custNum => {
+    // TODO: Get a real access token
     // Get this from here for now:
     // http://se-wwwa701.qa.mdgapp.net:12480/tools/oauth/samlgranttester?userId=testuser&clientId=nab&issuer=http%3A%2F%2Fnabapidev3.nabdev.com.au&scope=nab-mobile&attrs%5B0%5D.Key=&attrs%5B0%5D.Value=&attrs%5B1%5D.Key=&attrs%5B1%5D.Value=&attrs%5B2%5D.Key=&attrs%5B2%5D.Value=&attrs%5B3%5D.Key=&attrs%5B3%5D.Value=&attrs%5B4%5D.Key=&attrs%5B4%5D.Value=&attrs%5B5%5D.Key=&attrs%5B5%5D.Value=&generate=true
     return 'VQWm3QEWnQq2xJclEqqhlEIII9Nk';
@@ -27,7 +28,6 @@ const buildUrl = (custNum, endpoint, queryParams) => {
 
 module.exports = {
     get: (custNum, endpoint, queryParams) => {
-
         return axios({
             method: 'get',
             responseType: 'json',
@@ -38,9 +38,13 @@ module.exports = {
             console.log(`Response recieved ${response.status} ${response.statusText}`);
 
             if (response.status == 200) { return Promise.resolve(response.data.data); }
+            // TODO: Error handling / logging
         })
         .catch(error => {
             console.log("Error *** : " + error);
         });
     },
+    put: () => {},
+    post: () => {},
+    delete: () => {},
 }
--- data_access/tester.js
+++ data_access/tester.js
@@ -2,7 +2,7 @@ import { get } from './baseDataAccess';
 
 module.exports = {
     getTestData: (custNum, queryParams) => {
-        const endpoint = '/stocks/header';
+        const endpoint = '/stocks/header'; // TODO: make this into a constant
 
         return get(custNum, endpoint, queryParams)
             .then(response => {
